f89f8207e87bea4cf10ddbb2a334878f35529f6e
root <root@BOOK-8H3EFF2D96>
2026-05-01 16:31:15 +0900

    Print odd even and prime numbers

--- 5370.c
+++ 5370.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 
+int isPrime(int n) {
+    if (n < 2) {
+        return 0;
+    }
+
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
 int main(void) {
     printf("Odd numbers between 1 and 100:\n");
     for (int i = 1; i <= 100; i += 2) {
@@ -9,6 +23,13 @@ int main(void) {
     printf("Even numbers between 1 and 100:\n");
     for (int i = 2; i <= 100; i += 2) {
         printf("%d\n", i);
+    }
+
+    printf("Prime numbers between 1 and 100:\n");
+    for (int i = 1; i <= 100; i++) {
+        if (isPrime(i)) {
+            printf("%d\n", i);
+        }
     }
 
     return 0;
