e56a7c85118a0b0d3400731c673babe68ca6861e
root <root@BOOK-8H3EFF2D96>
2026-05-01 16:06:16 +0900

    Print odd and even numbers

--- 5370.c
+++ 5370.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
 int main(void) {
-    for (int i = 1; i <= 100; i++) {
-        if (i % 2 == 1) {
-            printf("%d\n", i);
-        }
+    printf("Odd numbers between 1 and 100:\n");
+    for (int i = 1; i <= 100; i += 2) {
+        printf("%d\n", i);
+    }
+
+    printf("Even numbers between 1 and 100:\n");
+    for (int i = 2; i <= 100; i += 2) {
+        printf("%d\n", i);
     }
 
     return 0;
